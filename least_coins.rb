dd14b6339a9e75764e09dc6aabf2df690477b33b
Sarah Li <sarcl@hotmail.com>
2018-06-22 13:02:08 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,12 @@
 #write out your code here
 
-def least_coins(cents)
-
+def (least_coins(29))
 #Code your answer here!
+coins_hash = {:quarters=>25, :dimes=>10,:nickels=>5,:pennies=>1}
+if least_coins >= 25
+  coins/25 = quarters
+  coins%25 = remainder
+elsif 
+
 
 end
